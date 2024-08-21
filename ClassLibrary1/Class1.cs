ad8fb4882b3a3248b7095e267fe385f54307b6c5
moonheart <themoonheart@outlook.com>
2024-08-21 18:57:05 +0800

    commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,50 @@
+﻿using ArxOne.MrAdvice.Advice;
+
+namespace ClassLibrary1;
+
+public class GenericAdvice<TReturn>: Attribute, IMethodAdvice
+{
+    public void Advise(MethodAdviceContext context)
+    {
+        if (context.IsTargetMethodAsync)
+        {
+            context.ReturnValue = Task.FromResult((object)2);
+        }
+        else
+        {
+            context.ReturnValue = 2;
+        }
+    }
+}
+
+public class NonGenericAdvice: Attribute, IMethodAdvice
+{
+    public void Advise(MethodAdviceContext context)
+    {
+        if (context.IsTargetMethodAsync)
+        {
+            context.ReturnValue = Task.FromResult((object)2);
+        }
+        else
+        {
+            context.ReturnValue = 2;
+        }
+    }
+}
+
+public class MyClass2
+{
+    [GenericAdvice<List<int>>]
+    public async Task<int> Method1()
+    {
+        await Task.Delay(1);
+        return 1;
+    }
+    
+    [NonGenericAdvice]
+    public async Task<int> Method2()
+    {
+        await Task.Delay(1);
+        return 1;
+    }
+}
---
+++ MrAdviceIssue/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClassLibrary1;
+
+var myClass1 = new MyClass1();
+Console.WriteLine($"await myClass1.Method1() = {await myClass1.Method1()}");
+Console.WriteLine($"await myClass1.Method2() = {await myClass1.Method2()}");
+
+var myClass2 = new MyClass2();
+Console.WriteLine($"await myClass2.Method1() = {await myClass2.Method1()}");
+Console.WriteLine($"await myClass2.Method2() = {await myClass2.Method2()}");
+
+public class MyClass1
+{
+    [GenericAdvice<List<int>>]
+    public async Task<int> Method1()
+    {
+        await Task.Delay(1);
+        return 1;
+    }
+    
+    [NonGenericAdvice]
+    public async Task<int> Method2()
+    {
+        await Task.Delay(1);
+        return 1;
+    }
+}
